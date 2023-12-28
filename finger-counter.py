a6c4fc223d49634483042af81f015ee922b14019
sksarvesh007 <bt22csd002@iiitn.ac.in>
2023-12-28 23:29:32 +0530

    changes

--- finger-counter.py
+++ finger-counter.py
@@ -4,28 +4,48 @@ import time
 import handtrackingmodule as htm
 import numpy as np
 import os
+ptime =0
 wcam , hcam = 640 , 480
 cap = cv2.VideoCapture(0)
 cap.set(3 , wcam)
 cap.set(4 , hcam)
-ptime =0
-
-folderpath = "finger images"
+folderpath = "finger_images"
 myList=os.listdir(folderpath)
-print(myList)
 overlaylist=[]
 for impath in myList:
     image = cv2.imread(f'{folderpath}/{impath}')
-    print(f'{folderpath}/{impath}')
     overlaylist.append(image)
+detector=htm.handDetector(detectionCon=0.75)
+tipids=[4,8,12,16,20]
 while True :
     success , img = cap.read()
-    img = img.resize((200,200))
-    img[0:200 , 0:200] = overlaylist[0]
+    img = detector.findhands(img)
+    lmlist = detector.findPosition(img , draw=False)
+    if len(lmlist)!=0:
+        fingers=[]
+        #thumb
+        if lmlist[tipids[0]][1]>lmlist[tipids[0]-1][1]:
+            fingers.append(1)
+        else :
+            fingers.append(0)
+        for id in range(1,5):
+            if lmlist[tipids[id]][2]<lmlist[tipids[id]-2][2]:
+                fingers.append(1)
+            else :
+                fingers.append(0)
+        #print(fingers)
+        totalfingers = fingers.count(1)
+        h , w, c= overlaylist[totalfingers-1].shape
+        img[0:h , 0:w] = overlaylist[totalfingers-1]
+        
+        cv2.rectangle(img , (20,225) , (170,425) , (0,255,0) , cv2.FILLED)
+        cv2.putText(img , str(totalfingers) , (45,375) , cv2.FONT_HERSHEY_PLAIN , 10 , (255,0,0) , 25)
     ctime = time.time()
-    fps=1/(ctime-ptime)
-    ptime=ctime
-    cv2.putText(img, f'FPS:{int(fps)}', (20, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 255, 0), 3)
+    fps= 1/(ctime-ptime)
+    ptime = ctime
+    cv2.putText(img , f'FPS:{int(fps)}' , (400,70) , cv2.FONT_HERSHEY_PLAIN , 3 , (255,0,255) , 3)
+    
+    ctime = time.time()
     cv2.imshow("Image" , img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+if fingerup == [0, 1, 0, 0, 0]:
+            print("1")
+        if fingerup == [0, 1, 1, 0, 0]:
+            print("2")
+        if fingerup == [0, 1, 1, 1, 0]:
+            print("3")
+        if fingerup == [0, 1, 1, 1, 1]:
+            print("4")
+        if fingerup == [1, 1, 1, 1, 1]:
+            print("5")
+        if fingerup == [1, 0, 0, 0, 0]:
+            print("6")
+        if fingerup == [1, 1, 0, 0, 0]:
+            print("7")
+        if fingerup == [1, 1, 1, 0, 0]:
+            print("8")
+        if fingerup == [1, 1, 1, 1, 0]:
+            print("9")
